1c598e90753b469703bbee8984f2c6cdba607776
Michal Trubini <mchl.trubini@gmail.com>
2023-10-19 14:36:32 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: { extend: {
+    colors: {
+      'dark-red': '#640032',
+      'purple': '#270041',
+      'dirty-orange': '#ce5c00',
+      'modal': 'rgba(0, 0, 0, 0.8)'
+    },
+  } },
+  variants: { extend: {} },
+  plugins: [],
+}
